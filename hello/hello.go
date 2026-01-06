8d44df450f6489351673ed13794041e3df239da1
mi5ha6in <mi5ha6in@gmail.com>
2026-01-06 05:05:51 +0300

    handle-errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Dmitrii")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Dmitrii")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
