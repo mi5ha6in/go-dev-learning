c4e6fff072c2c75be9e482c3a5e2cb14c5d7603c
mi5ha6in <mi5ha6in@gmail.com>
2026-08-04 23:25:44 +0300

    adds more test for text-analyzer

--- text-analyzer/text-analyzer_test.go
+++ text-analyzer/text-analyzer_test.go
@@ -6,19 +6,59 @@ import (
 )
 
 func TestSplitWords(t *testing.T) {
-	text := "Go, кто-то сказал: don't stop."
-
-	got := splitWords(text)
-
-	want := []string{
-		"go",
-		"кто-то",
-		"сказал",
-		"don't",
-		"stop",
+	tests := []struct {
+		name string
+		text string
+		want []string
+	}{
+		{
+			name: "пунктуация",
+			text: "Go, Go! Go.",
+			want: []string{"go", "go", "go"},
+		},
+		{
+			name: "дефис внутри слова",
+			text: "Кто-то пришёл",
+			want: []string{"кто-то", "пришёл"},
+		},
+		{
+			name: "апостроф внутри слова",
+			text: "Don't stop",
+			want: []string{"don't", "stop"},
+		},
+		{
+			name: "дефис по краям",
+			text: "-word test-",
+			want: []string{"word", "test"},
+		},
+		{
+			name: "двойной дефис",
+			text: "rock--roll",
+			want: []string{"rock", "roll"},
+		},
+		{
+			name: "цифры",
+			text: "Go123 version-2",
+			want: []string{"go123", "version-2"},
+		},
+		{
+			name: "пустая строка",
+			text: "",
+			want: []string{},
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := splitWords(tt.text)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("splitWords(%q) = %#v, want %#v", text, got, want)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf(
+					"splitWords(%q) = %#v, want %#v",
+					tt.text,
+					got,
+					tt.want,
+				)
+			}
+		})
 	}
 }
